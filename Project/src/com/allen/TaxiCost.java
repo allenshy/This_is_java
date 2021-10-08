fb5db304208525d24614adc691349e3e5bee6337
allenshy <zzzallen2009@gmail.com>
2021-10-08 17:04:42 +0800

    none

---
+++ Project/src/com/allen/TaxiCost.java
@@ -0,0 +1,25 @@
+package com.allen;
+
+import java.util.Scanner;
+
+public class TaxiCost {
+
+	public static void main(String[] args) {
+
+		System.out.println("搭乘時間  日間(1)，夜間(2)");
+
+		Scanner scanner = new Scanner(System.in);
+
+		int time;
+		float km;
+		int cost;
+
+		time = scanner.nextInt();
+		System.out.println("里程(km)？");
+		km = scanner.nextFloat();
+
+		
+		}
+	
+
+}
